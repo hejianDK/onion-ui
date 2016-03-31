f533849189d17ec4ad3200a35e3ceccf625c4aaa
Zifan Zhang <zifan90@gmail.com>
2016-03-31 01:12:32 -0400

    removed invalid test

--- test/soysauce.test.js
+++ test/soysauce.test.js
@@ -1,15 +1,8 @@
 import {expect} from 'chai';
-import {SyncObject, SyncMode} from '../app/reducers/SyncObject';
+import {SyncObject, SyncMode} from '../app/reducers/StateHolder';
 
 describe('grade 1 math', () => {
   it('1 plus 1 should equal 2', () => {
     expect(1+1).to.equal(2);
   });
-
-  it('test', () => {
-    const s1 = new SyncObject(SyncMode.READING, {a: 1});
-    const s2 = new SyncObject(SyncMode.NONE, undefined, {e: 1});
-
-    console.log(Object.assign({}, s1, s2));
-  })
 });
