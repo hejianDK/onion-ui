7dc52abdc1c673bd985fe59b2cc085cf0aff5a55
Zifan Zhang <zifan90@gmail.com>
2016-03-31 01:13:51 -0400

    added bootstrap webpack support

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,15 @@ const DEV_SERVER_PORT = require('./const.json').DEV_SERVER_PORT;
 
 module.exports = {
   name: 'client',
+  devtool: 'source-map',
   devServer: {
     historyApiFallback: true,
     hot: true,
     inline: true,
     progress: true,
+    stats: {
+      colors: true
+    },
     contentBase: './app',
     port: DEV_SERVER_PORT,
     proxy: {
@@ -20,11 +24,10 @@ module.exports = {
       }
     }
   },
-  entry: {
-    app: path.resolve(__dirname, 'app/main.js'),
-    vendor: ['react', 'react-dom', 'redux', 'react-redux', 'redux-logger', 'redux-thunk',
-      'react-router', 'isomorphic-fetch', 'es6-promise', 'lodash']
-  },
+  entry: [
+    'bootstrap-loader',
+    path.resolve(__dirname, 'app/main.js')
+  ],
   output: {
     path: __dirname + '/build',
     publicPath: '/',
@@ -47,18 +50,23 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader'
       },
-      { 
-        test: /\.(png|jpg|gif)$/, 
-        loader: 'url-loader'
-      }
+      { test: /\.(png|jpg|gif)$/, loader: 'url-loader' },
+      { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports?jQuery=jquery' },
+      { test: /\.(woff2?|svg)$/, loader: 'url?limit=10000' },
+      { test: /\.(ttf|eot)$/, loader: 'file' }
     ]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.optimize.CommonsChunkPlugin(
-      'vendor', 'vendor.js'
-    ),
-    new OpenBrowserPlugin({ url: `http://localhost:${DEV_SERVER_PORT}` })
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'commons',
+      filename: 'commons.js',
+      minChunks: 3,
+      chunks: ['react', 'react-dom', 'redux', 'react-redux', 'redux-logger', 'redux-thunk',
+        'react-router', 'isomorphic-fetch', 'es6-promise', 'lodash', 'jquery']
+    }),
+    new OpenBrowserPlugin({ url: `http://localhost:${DEV_SERVER_PORT}` }),
+    new webpack.optimize.DedupePlugin()
   ]
 };
 
